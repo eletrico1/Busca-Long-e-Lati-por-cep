d6de2ec6a7cd8bfbbe5323b914455be6fe9f8f01
allan.barreto <allanfernandesti@gmail.com>
2023-10-31 17:48:12 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+?>
+    <!DOCTYPE html>
+<!--
+  @license
+  Copyright 2019 Google LLC. All Rights Reserved.
+SPDX-License-Identifier: Apache-2.0
+-->
+<html>
+  <head>
+    <title>Geocoding Service</title>
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="style.css">;
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBSpSh9kJYO3X_luiZ3zccm_q_S3KaFqRo&callback=initMap&v=weekly"
+    defer
+    ></script>
+  teste
+  </body>
+</html>
